eec70e08e7566b99fc0406a88d91eefedb02d45f
JiinHong <com5942@seoultech.ac.kr>
2025-03-27 20:55:49 +0900

    git init

---
+++ src/main/java/org/example/stlog/STlogApplication.java
@@ -0,0 +1,13 @@
+package org.example.stlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(STlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stlog/STlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class STlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
